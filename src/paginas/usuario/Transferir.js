640ada322782103b3aa423e3e845f9fab9cb712b
Kansetsu <vinicius802010@gmail.com>
2022-09-26 17:42:04 -0300

    Adicionada função de transferência e adicionada integração com API

--- src/paginas/usuario/Transferir.js
+++ src/paginas/usuario/Transferir.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../App.css";
-
+import api from "../../services/api";
+import { useForm } from "react-hook-form";
 import { Form, Card } from "react-bootstrap";
 
 function Transferir() {
+
+  const { handleSubmit, register } = useForm();
+  console.log("REGISTER:", register)
+  const onSubmit = (data) => {
+    if (data) {
+      api.put(`atualizarAcolhido/${data.matricula}`, {
+        ...data,
+      })
+      alert('Transferido com sucesso!!');
+    } else {
+      alert('Houve algum erro no sistema, tente novamente.');
+    }
+
+  };
+
   return (
     <>
       <Card>
@@ -12,32 +28,24 @@ function Transferir() {
         </Card.Header>
         <Card.Body className="card-body">
           <div className="container">
-            <input
-              className="form-control campo-transferir"
-              type="text"
-              placeholder="Matrícula"
-              id="consulta-matricula"
-              name="consulta-matricula"
-            />
-            <Form.Select className="campo-transferir">
-              <option>Unidade de origem</option>
-              <option value="origem-recife-centro">Recife - Centro</option>
-              <option value="origem-recife-jardim-sp">
-                Recife - Jardim São Paulo
-              </option>
-              <option value="origem-paudalho">Paudalho</option>
-              <option value="origem-jaboatao">Jaboatão dos Guararapes</option>
-            </Form.Select>
-            <Form.Select className="campo-transferir">
-              <option>Unidade de destino</option>
-              <option value="destino-recife-centro">Recife - Centro</option>
-              <option value="destino-recife-jardim-sp">
-                Recife - Jardim São Paulo
-              </option>
-              <option value="destino-paudalho">Paudalho</option>
-              <option value="destino-jaboatao">Jaboatão dos Guararapes</option>
-            </Form.Select>
-            <button class="submit-btn">Transferir</button>
+            <form onSubmit={handleSubmit(onSubmit)} id="form">
+              <input
+                className="form-control campo-transferir"
+                type="text"
+                placeholder="Matrícula"
+                id="consulta-matricula"
+                name="consulta-matricula"
+                {...register("matricula")}
+              />
+              <Form.Select className="campo-transferir" {...register("unidadeDeOrigem")}>
+                <option disabled selected> --- Selecione a Unidade de Destino --- </option>
+                <option value="Recife - Centro" >Recife - Centro</option>
+                <option value="Recife - Jardim São Paulo" > Recife - Jardim São Paulo</option>
+                <option value="Paudalho" >Paudalho</option>
+                <option value="Jaboatão dos Guararapes" >Jaboatão dos Guararapes</option>
+              </Form.Select>
+              <button class="submit-btn" type="submit">Transferir</button>
+            </form>
           </div>
         </Card.Body>
       </Card>
